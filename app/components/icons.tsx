f588d0b209f337af00089d5f576bee03af956077
moaqz <goz.matu@gmail.com>
2023-10-26 10:34:16 -0300

    feat(navbar): design improvements

--- app/components/icons.tsx
+++ app/components/icons.tsx
@@ -14,20 +14,6 @@ export function HomeIcon(props: SVGAttributes<SVGElement>) {
   );
 }
 
-export function UserCircleIcon(props: SVGAttributes<SVGElement>) {
-  return (
-    <svg
-      aria-hidden="true"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="currentColor"
-      viewBox="0 0 20 20"
-      {...props}
-    >
-      <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
-    </svg>
-  );
-}
-
 export function ImageIcon(props: SVGAttributes<SVGElement>) {
   return (
     <svg
@@ -75,27 +61,6 @@ export function LinkIcon(props: SVGAttributes<SVGElement>) {
         strokeWidth="2"
         d="M11.013 7.962a3.519 3.519 0 0 0-4.975 0l-3.554 3.554a3.518 3.518 0 0 0 4.975 4.975l.461-.46m-.461-4.515a3.518 3.518 0 0 0 4.975 0l3.553-3.554a3.518 3.518 0 0 0-4.974-4.975L10.3 3.7"
       />
-    </svg>
-  );
-}
-
-export function LogOutIcon(props: SVGAttributes<SVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
-      <polyline points="16 17 21 12 16 7" />
-      <line x1="21" x2="9" y1="12" y2="12" />
     </svg>
   );
 }
--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,17 +1,16 @@
 import { Link, useLocation } from "@remix-run/react";
-import { HomeIcon, LogOutIcon, UserCircleIcon } from "./icons";
-import { SignOutButton, useUser } from "@clerk/remix";
+import { HomeIcon } from "./icons";
+import { UserButton } from "@clerk/remix";
 
 export default function Navbar() {
-  const { user } = useUser();
   const location = useLocation().pathname;
 
   return (
     <nav className="fixed bottom-0 left-0 z-50 bg-gray-950 border-t border-t-gray-800 w-full sm:relative sm:border-t-0 sm:z-auto">
-      <div className="flex items-center sm:flex-col sm:sticky sm:top-0">
+      <div className="px-6 flex justify-between items-center sm:min-h-screen sm:px-0 sm:justify-between sm:flex-col sm:sticky sm:top-0 sm:py-6">
         <Link
           to="/"
-          className="h-16 w-16 flex items-center justify-center text-gray-300"
+          className=" flex items-center justify-center text-gray-300 focus:outline focus:outline-2 focus:outline-blue-600 focus:outline-offset-4 rounded-md"
           title="Home"
         >
           <HomeIcon
@@ -21,26 +20,9 @@ export default function Navbar() {
           />
         </Link>
 
-        <Link
-          to={`/${user?.username}`}
-          className="h-16 w-16 flex items-center justify-center text-gray-300"
-          title="Profile"
-        >
-          <UserCircleIcon
-            className={`w-6 h-6 ${
-              location === `/${user?.username}` ? "text-white" : "opacity-50"
-            }`}
-          />
-        </Link>
-
-        <SignOutButton>
-          <button
-            className="h-16 w-16 flex items-center justify-center text-gray-300"
-            title="Logout"
-          >
-            <LogOutIcon className="w-6 h-6 opacity-50" />
-          </button>
-        </SignOutButton>
+        <div className="h-16 w-16 flex items-center justify-center">
+          <UserButton />
+        </div>
       </div>
     </nav>
   );
